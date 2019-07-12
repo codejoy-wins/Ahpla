aababfcd286c38ea470e341fd829b2fcf7a1cc80
Max <mjann@ucsc.edu>
2019-07-12 14:54:44 -0700

    time app

--- src/components/Time.js
+++ src/components/Time.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 function Time(){
     const date = new Date(); // creating date object?
+    const hours = date.getHours();
+    let timeOfday = "error";
+
+    if(hours < 12){
+        timeOfday = "Good Morning";
+    } else if (hours < 17){
+        timeOfday = "Good Afternoon";
+    } else{
+        timeOfday = "Goodnight";
+    }
+
     return(
-        <div>
-            <p>Year: {date.getFullYear()}</p>
-            <p>Month: {date.getMonth()+1}</p>
-            <p>Day: {date.getDate()}</p>
+        <div style={{textAlign: "center"}}>
             <p>Date: {date.getMonth()+1}/{ date.getDate()}/{date.getFullYear()}</p>
+            <p>{timeOfday}</p>
             <p>It is currently about {date.getHours() % 12} o' clock!</p>
         </div>
     )
